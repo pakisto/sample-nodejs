acf352af611584620a6b87306f6ce6fd271df843
Changyu <changyu.seon@bespinglobal.com>
2018-03-27 14:55:29 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World! v1.0.3' });
+  res.render('index', { title: 'Hello World! v1.0.4' });
 });
 
 module.exports = router;
